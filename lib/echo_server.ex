43e06bb63f2f8da536df9acf83d7f3f67470190a
niku <niku@niku.name>
2016-06-16 00:23:15 +0900

    Listen on a tcp port

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -1,2 +1,8 @@
 defmodule EchoServer do
+  require Logger
+
+  def listen(port) do
+    :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Listen connection on port #{port}"
+  end
 end
