48818dc81312af6ddbdc295b8fedf2490f5264e3
niku <niku@niku.name>
2016-06-16 00:29:00 +0900

    Accept a tcp connection

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -2,7 +2,13 @@ defmodule EchoServer do
   require Logger
 
   def listen(port) do
-    :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    {:ok, listen_socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Listen connection on port #{port}"
+    accept(listen_socket)
+  end
+
+  defp accept(listen_socket) do
+    :gen_tcp.accept(listen_socket)
+    Logger.info "A connection accepted"
   end
 end
