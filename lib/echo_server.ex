7324dcdeaa5e9604b5fb6871303590faaaab07d9
niku <niku@niku.name>
2016-06-16 00:44:07 +0900

    Loop echo

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -10,13 +10,19 @@ defmodule EchoServer do
   defp accept(listen_socket) do
     {:ok, socket} = :gen_tcp.accept(listen_socket)
     Logger.info "A connection accepted"
-    read_line(socket)
+    echo(socket)
+  end
+
+  defp echo(socket) do
+    line = read_line(socket)
+    write_line(socket, line)
+    echo(socket)
   end
 
   defp read_line(socket) do
     {:ok, line} = :gen_tcp.recv(socket, 0)
     Logger.debug "#{inspect socket} receives #{line}"
-    write_line(socket, line)
+    line
   end
 
   defp write_line(socket, line) do
